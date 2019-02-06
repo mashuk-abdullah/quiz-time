0a3cbefd518d833958d95196f0d5eb85beb43485
Abdullah Al Mashuk <mashuk.cse.gub@gmail.com>
2019-02-06 14:20:44 +0600

    Add files via upload

---
+++ src/quizsystem/QuizSystem.java
@@ -0,0 +1,24 @@
+/**<
+ *  Name: Abdullah Al Mashuk 
+ *  Id: 151002009 >
+ */
+package quizsystem;
+
+public class QuizSystem {
+
+    public static void main(String[] args) {
+        
+        new Login().setVisible(true);
+//        new Registration().setVisible(true);
+//        
+//        new JoinPageAdmin().setVisible(true);
+//        new QuestionSetting().setVisible(true);
+//        new SearchQuestion().setVisible(true);
+//        new TimeSetting().setVisible(true);
+//        
+//        new JoinPageUser().setVisible(true);
+//        new QuestionPaper().setVisible(true);
+//        new Result().setVisible(true);
+//        new ResultBoard().setVisible(true);
+    }
+}
